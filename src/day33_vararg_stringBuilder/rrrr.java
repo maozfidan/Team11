00583b8da8c5a39ebc97516fc92def4cb349071f
M.Arif Özfidan <ozfidanarif@gmail.com>
2022-04-04 18:26:55 +0200

    04.04.2022

---
+++ src/day33_vararg_stringBuilder/rrrr.java
@@ -0,0 +1,17 @@
+package day33_vararg_stringBuilder;
+
+public class rrrr {
+
+    public static void main(String[] args) {
+
+        StringBuilder sb1 = new StringBuilder("OCAJP8");
+
+        sb1.subSequence(2, 4);
+
+        sb1.deleteCharAt(3);
+
+        sb1.reverse( );
+
+        System.out.println(sb1);
+    }
+}
